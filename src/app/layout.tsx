cfc7fce73270600c0bd355ac56457e581508fa20
José Gabriel Griffin Silva <32249455+gabriel14silva@users.noreply.github.com>
2024-05-09 17:36:54 -0600

    add-loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
 import { Roboto } from "next/font/google";
+import { Hero } from "app/components/home/Hero";
+import { Description } from "app/components/home/Description";
 import { Header } from "app/components/shared/Header";
 import { Footer } from "app/components/shared/Footer";
 import "app/sass/globals.sass";
 
 const roboto = Roboto({
-  weight: ["100", "100", "500", "700"],
+  weight: ["100", "300", "500", "700"],
   subsets: ["latin"],
 });
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={roboto.className}>
         <Header />
+        <Hero />
+        <Description />
         {children}
         <Footer />
       </body>
